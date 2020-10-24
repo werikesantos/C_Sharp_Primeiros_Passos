bb4f0575d11ad035de598e4273f3074d3f73d44b
Werikesantos <werike.ms@gmail.com>
2020-10-24 12:53:57 -0300

    areaTrapezio

---
+++ 03_EstruturaSequencial/03_areaTrapezio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace areaTrapezio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OBS: VARIÁVEIS DO TIPO 'DOUBLE' DEVEM TER O PONTO FLUTUANTE.
+            //OBS: VARIÁVEIS DO TIPO 'FLOAT' DEVEM TER O 'f' NO FINAL. EX: 'float a; a = 10f;
+
+            double area, h, b, B;
+
+            h = 5.0;
+            b = 6.0;
+            B = 8.0;
+
+            area = (((b + B) / 2.0) * h);
+
+            Console.WriteLine(area);
+
+            Console.ReadLine();
+        }
+    }
+}
