00c6b8a3c4fc40af0787a4d4fcfe7041c721de25
Werikesantos <werike.ms@gmail.com>
2020-10-24 17:49:30 -0300

    Exercício de Fixação

---
+++ 03_EstruturaSequencial/07_exercicioFixacao_EntradaESaidaDeDados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07_exercicioFixacao_EntradaESaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int qtdQuarto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double precoProduto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o seu último nome, idade e altura (mesma linha e separados com espaços):");
+            String[] dados = Console.ReadLine().Split(' ');
+            String ultimoNome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2]);
+
+            Console.WriteLine(nome + "\n" + qtdQuarto + "\n" + precoProduto.ToString("F2") + "\n" + ultimoNome + "\n" + idade + "\n" + altura);
+        }
+    }
+}
