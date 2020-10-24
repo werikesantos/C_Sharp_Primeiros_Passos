515c03be5efc1b99b647f59ec49c30925d14c3c6
Werikesantos <werike.ms@gmail.com>
2020-10-24 15:52:59 -0300

    Como realizar Saídas de Dados

---
+++ 03_EstruturaSequencial/04_saidaDeDados/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//BIBLIOTECA PARA FORMATAR A UTILIZAÇÃO DO PONTO FLUTUANTE '.'
+using System.Globalization;
+
+namespace _04_saidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+
+            //INPRIMINDO O DADO:
+            Console.WriteLine(x);
+            //IMPRIMINDO O DADO COM DUAS CASAS DECIMAIS:
+            Console.WriteLine(x.ToString("F2"));
+            //IMPRIMINDO O DADO COM 4 CASAS DECIMAIS:
+            Console.WriteLine(x.ToString("F4"));
+            //IMPRIMINDO O DADO COM '.' AO INVÉS DA UTILIZAÇÃO DA ','.
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
