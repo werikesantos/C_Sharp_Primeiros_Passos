16e007e03f6d1e52989be5487a25f4d2e3f221f8
Werikesantos <werike.ms@gmail.com>
2020-10-24 12:51:34 -0300

    Olá, Mundo!

---
+++ 03_EstruturaSequencial/01_olaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo!");
+        }
+    }
+}
